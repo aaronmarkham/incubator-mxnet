906eea5268776efa737a053d664aaa69dba317a0
Aaron Markham <markhama@amazon.com>
2018-09-27 12:21:58 -0700

    let search be remembered through version switches

--- docs/_static/js/search.js
+++ docs/_static/js/search.js
@@ -1,9 +1,28 @@
-$(document).ready(function () {
+function setVersion(){
+        let doc = window.location.pathname.match(/^\/(search\/.*)$/) || window.location.pathname.match(/^\/versions\/[^*]+\/(search\/.*)$/);
+        if (doc) {
+            let params = (new URL(document.location)).searchParams || '';
+            console.log('params: ', params);
+            if (document.getElementById('dropdown-menu-position-anchor-version')) {
+                    versionNav = $('#dropdown-menu-position-anchor-version a.main-nav-link');
+                    $(versionNav).each( function( index, el ) {
+                            currLink = $( el ).attr('href');
+                            version = currLink.match(/\/versions\/([0-9.master]+)\//);
+                            if (version) {
+                                    versionedDoc = '/versions/' + version[1] + '/' + doc[1] + (window.location.hash || params || '');
+                                    $( el ).attr('href', versionedDoc);
+                            }
+                    });
+            }
+        }
+}
+
+function setSearch(){
     var searchForm = $("#search-input-wrap").children("form").first();
     searchForm.append('<div class="form-group searchBtn"><input type="submit" class="form-control" value="Go"></div>');
     searchForm.children("div").first().addClass("searchBox");
     $(".searchBox").addClass("searchBoxNorm");
-    
+
     $('#searchIcon').click(function () {
         if($('#search-input-wrap').is(':hidden')) {
             $('#search-input-wrap').show();
@@ -16,4 +35,9 @@ $(document).ready(function () {
             $('#searchIcon span').addClass('glyphicon-search');
         }
     });
+}
+
+$(document).ready(function () {
+    setSearch();
+    setVersion();
 });
